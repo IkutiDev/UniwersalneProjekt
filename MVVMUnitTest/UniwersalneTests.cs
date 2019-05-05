724ff47026b970bd8b88454fa45b6722b6d140fa
Michał <m.j.olkiewicz@gmail.com>
2019-05-05 20:00:07 +0200

    Dodanie projektu dla testow

---
+++ MVVMUnitTest/UniwersalneTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Moq;
+using UniwersalneProjekt.Models;
+using UniwersalneProjekt.Services;
+using UniwersalneProjekt.ViewModels;
+using Xunit;
+
+namespace MVVMUnitTest
+{
+    public class UniwersalneTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
